ad66178fa1e590fad592132ddea151680f2051bf
JahedHossenBangladesh <jahedhossen4358@gmail.com>
2023-06-23 01:17:06 +0600

    assing the value with out space

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 # Ask the your name
 name = input("what is you name")
+
+name = name.strip()
 #Ans the input
 """
 this is the long comment
@@ -7,3 +9,4 @@ of the py
 """
 print ("my name is " + name) 
 print ("My name is ",name)
+print (f"name is {name}")
