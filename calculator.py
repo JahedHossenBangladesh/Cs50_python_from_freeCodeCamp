38b30d0e18b8e069ff34e3b5cad2fae2eefd8d8e
JahedHossenBangladesh <jahedhossen4358@gmail.com>
2023-06-23 01:40:46 +0600

    we learn to use the int

---
+++ calculator.py
@@ -0,0 +1,5 @@
+z=input("whit is x?")
+y=input("what is y?")
+x = int(z) +int(y)
+
+print (x)
--- hello.py
+++ hello.py
@@ -15,11 +15,13 @@ name = name.strip().title()
 """
  #more short the line from 1 to 13 line 
  
-name = input("what is your name").strip().title()
+# name = input("what is your name").strip().title()
 
-#split user's name into first name and last name
-first, last = name.split(" ")
+# #split user's name into first name and last name
+# first, last = name.split(" ")
 
-print ("my name is " + name) 
-print ("My name is ",last)
-print (f"name is {first}")
+# print ("my name is " + name) 
+# print ("My name is ",last)
+# print (f"name is {first}")
+
+#this is the mathmatical operator or int and float
