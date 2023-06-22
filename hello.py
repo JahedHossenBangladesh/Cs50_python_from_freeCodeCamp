275af5ab75cd16dfb4ef50d618fb89a8d91d999c
JahedHossenBangladesh <jahedhossen4358@gmail.com>
2023-06-23 01:25:03 +0600

    short the code

--- hello.py
+++ hello.py
@@ -1,12 +1,22 @@
+"""
 # Ask the your name
 name = input("what is you name")
-
+#remove the space
 name = name.strip()
-#Ans the input
+#Capitalize the name
+name = name.capitalize()
+
+#gramaticaly correct
+name = name.title()
+
+
+#shortage the code from 4 to 9
+name = name.strip().title()
 """
-this is the long comment
-of the py
-"""
+ #more short the line from 1 to 13 line 
+ 
+name = input("what is your name").strip().title()
+
 print ("my name is " + name) 
 print ("My name is ",name)
 print (f"name is {name}")
