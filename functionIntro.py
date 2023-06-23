82602bc2af6f9a679390cb9ba03af2b5a2117d9b
JahedHossenBangladesh <jahedhossen4358@gmail.com>
2023-06-23 19:59:37 +0600

    main function is the global function to show the later diclar function which is use for the

--- functionIntro.py
+++ functionIntro.py
@@ -1,7 +1,14 @@
-def hello():
-    print ("Hello")
+# def hello():
+#     print ("Hello")
     
 
-name = input ("what is your name?").strip().title()
+# name = input ("what is your name?").strip().title()
+# hello()
+# print(name)
+
+def hello(to='world'):
+    print("hello,",to)
 hello()
-print(name)
+
+name = input("What's your name? ").title()
+hello(name)
---
+++ mainFunction.py
@@ -0,0 +1,16 @@
+def main():
+    name= input("What's your name ?").title()
+    hello(name)
+
+
+def hello(to="world"):
+    print("Hello, ",to)
+    
+    
+    
+    
+    
+    
+    
+    
+main()
