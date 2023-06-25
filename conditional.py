910009e28a081c54cc3f77099281a2ef8457427f
JahedHossenBangladesh <jahedhossen4358@gmail.com>
2023-06-25 19:53:56 +0600

    this is the elif

--- conditional.py
+++ conditional.py
@@ -2,7 +2,7 @@ x=int(input("what is x? "))
 y = int(input("what is y?"))
 if x<y:
     print("x is less then y")
-if x > y:
+elif x > y:
     print("x is grater then y")
-if x == y:
+elif x == y:
     print("x is equal to y")
