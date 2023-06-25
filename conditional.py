a2d2ed37716a656a890e134f8c1a166009e644a1
JahedHossenBangladesh <jahedhossen4358@gmail.com>
2023-06-25 20:25:30 +0600

    not equal or equal

--- conditional.py
+++ conditional.py
@@ -1,8 +1,14 @@
 x=int(input("what is x? "))
 y = int(input("what is y?"))
-if x<y:
-    print("x is less then y")
-elif x > y:
-    print("x is grater then y")
+# if x<y:
+#     print("x is less then y")
+# elif x > y:
+#     print("x is grater then y")
+# else :
+#     print("they are equal")
+    
+if x !=y :
+    print("is not equal")
 else :
-    print("they are equal")
+    print("is equal")
+    
