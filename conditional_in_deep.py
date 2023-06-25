ed5d54bf0974ca66728b289a16f2829eeffe4356
JahedHossenBangladesh <jahedhossen4358@gmail.com>
2023-06-25 20:48:45 +0600

    tricky conditional

---
+++ conditional_in_deep.py
@@ -0,0 +1,36 @@
+score = int(input("what is your score"))
+
+if score >=90:
+    print("A")
+elif score >=80:
+    print("b")
+elif score >=70:
+    print("c")
+elif score >=60:
+    print("d")
+else:
+    print("f")
+    
+if score >=90 and score <=100:
+    print("a")
+elif score >= 80 and score <90:
+    print("b")
+elif score >=70 and score <80:
+    print("c")
+elif score >=60 and score < 70:
+    print("d")
+else:
+    print("f")
+    
+    
+    
+if  90 <= score <=100:
+    print("a")
+elif  80<= score <=90:
+    print("b")
+elif 70 <= score <=80:
+    print("c")
+elif 60<= score <= 70:
+    print("d")
+else:
+    print("f")
