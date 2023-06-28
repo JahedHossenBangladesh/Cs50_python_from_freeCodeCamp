859415e4190ae974816fc9a7d48c90c5b28ad618
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2023-06-28 20:59:45 +0600

    more tricky if and else for the true and false

--- party.py
+++ party.py
@@ -16,11 +16,15 @@ def main():
 #         return True
 #     else:
 #         return False
+    
+    
     #the expart option
+# def is_even(n):
+#     return True if n % 2 == 0 else False       
+        
+        #More tricky
 def is_even(n):
-    return True if n % 2 == 0 else False       
-        
-        
+    return n % 2 == 0
         
         
         
