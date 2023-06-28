15b85632a45cbb9df5396b7da7b5fb827e1b1384
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2023-06-28 23:30:37 +0600

    this is for match

---
+++ match.py
@@ -0,0 +1,8 @@
+name=input("what is your name")
+
+if name == "Joshim" or name =="manna" or name == "Rubel":
+    print("Daliwood")
+elif name =="sharuk" or name == "Salman":
+    print("Bollowood")
+else:
+    print("who?")
