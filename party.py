0eb90b0a876962d904505dd6544e26662ef5bda6
JahedHossenBangladesh <mokbulhossen4358@gmail.com>
2023-06-28 20:38:38 +0600

    this is new

---
+++ party.py
@@ -0,0 +1,20 @@
+# x=int(input("what is the number?"))
+
+# if x % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+def main():
+    x=int(input("what is the number?"))
+    if is_even(x):
+        print("even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+        
+main()
