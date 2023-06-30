d1351ba1994f641d8df41a35ddaaa6c4ca8d0e20
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2023-06-30 22:11:11 +0600

    new

---
+++ match.py
@@ -0,0 +1,41 @@
+name=input("what is your name? ")
+
+match name:
+    case  "Manna" | "Jahsim" | "Rubel":
+        print("Daliwood")
+    case "Saruk"|"Salman"|"Abraham":
+        print("Bolliwood")
+    case _:
+        print("Who?")
+
+
+
+
+
+
+
+
+
+
+# match name:
+#     case "Joshim":
+#         print("Dali")
+#     case "Manna":
+#         print("Dali")
+#     case "Rubel":
+#         print("Dali")
+#     case "Saruk":
+#         print("Boli")
+#     case _:
+#         print("Who?")
+
+
+
+
+# if name == "Joshim" or name =="manna" or name == "Rubel":
+#     print("Daliwood")
+# elif name =="sharuk" or name == "Salman":
+#     print("Bollowood")
+# else:
+#     print("who?")
+
