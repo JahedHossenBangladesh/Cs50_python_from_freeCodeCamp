6cad603e52e06ee4ef4b3e760822e9c20b79bf9a
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2023-07-04 21:01:02 +0600

    tricky for and while loop

--- cat2.py
+++ cat2.py
@@ -1,10 +1,20 @@
-for i in [0,1,2]:
-    print("meow")
+# for i in [0,1,2]:
+#     print("meow")
  
 #  for i in range(3):
 #      print("go")
     
-for _ in range(3):
-    print("bu")
+# for _ in range(3):
+#     print("bu")
     
-print("meow\n" *3,end="")
+# print("meow\n" *3,end="")
+
+while True:
+    n =int(input("What is n?"))
+    if n >0:
+        break
+for _ in range(n):
+    print("meow")
+
+
+
