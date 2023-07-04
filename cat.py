072273876ccedd8f0270533f0cb422728df1d916
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2023-07-04 20:46:12 +0600

    this for and while loop

--- cat.py
+++ cat.py
@@ -1,3 +1,11 @@
-print("maow")
-print("maow")
-print("maow")
+# i= 3
+# while  i !=0:
+#     print("meow")
+#     i=i-1
+
+i = 0
+
+while i< 3:
+    print("meow")
+    # i = i +1
+    i +=1
---
+++ cat2.py
@@ -0,0 +1,10 @@
+for i in [0,1,2]:
+    print("meow")
+ 
+#  for i in range(3):
+#      print("go")
+    
+for _ in range(3):
+    print("bu")
+    
+print("meow\n" *3,end="")
