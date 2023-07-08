278ceacb8e665800a7a2f9f6baf51b2ec5731011
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2023-07-08 16:06:50 +0600

    @

--- mario2.py
+++ mario2.py
@@ -3,13 +3,24 @@
     
 def main():
     print_column(3)
+    print_row(4)
+    print_squire(6)
+    
 def print_column(height):
-    print("#\n" * height,end="")
+    # print("#\n" * height,end="")
     for _ in range(height):
         print("#")
+        
+def print_row(width):
+    print("?" * width)
 
 
-
+def print_squire(size):
+    for _ in range(size):
+        for j in range(size):
+            print("#",end="")
+        print()
+        
 
 
 
